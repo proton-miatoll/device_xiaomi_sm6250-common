45453e583d9f49c9031cc8375ca593f404ca89bc
JamieHoSzeYui <henloboii@protonmail.ch>
2021-06-27 19:14:46 +0800

    sm6250-common: initialize vendorsetup
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# Drop healthd and clone LOS one
+rm -rf vendor/qcom/opensource/healthd-ext 
+git clone --depth=1 --single-branch https://github.com/LineageOS/android_vendor_qcom_opensource_healthd-ext vendor/qcom/opensource/healthd-ext 
+
+# Clone hardware/xiaomi if it doesn't exist
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi 
