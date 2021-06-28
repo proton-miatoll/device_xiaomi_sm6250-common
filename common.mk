9e813c13a758ac7b3ea747603a66a42d7513d433
JamieHoSzeYui <henloboii@protonmail.ch>
2021-06-28 17:55:35 +0800

    sm6250-common: drop XiaomiDoze
    * it's inaccurate anyways.
    
    Signed-off-by: JamieHoSzeYui <henloboii@protonmail.ch>
    Change-Id: Ibd99a93635104a6cd36790f5f4438801a4dcb7f4

--- common.mk
+++ common.mk
@@ -157,10 +157,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
