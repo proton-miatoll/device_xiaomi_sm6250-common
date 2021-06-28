f631deeb7ef60704f1345f30b04187fc764a7f74
JamieHoSzeYui <henloboii@protonmail.ch>
2021-06-28 16:29:52 +0800

    sm6250-common: vendorsetup: clone CAF telephony
    Signed-off-by: JamieHoSzeYui <henloboii@protonmail.ch>
    Change-Id: I1a166a9df1d30b4dbc2b1576b77fa4292c0e1f51

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,6 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 # QCOM sepolicy
 git clone https://github.com/PixelExperience/device_qcom_sepolicy_vndr device/qcom/sepolicy_vndr/
 git clone https://github.com/PixelExperience/device_qcom_sepolicy device/qcom/sepolicy/
+
+# QCOM Telephony
+git clone https://github.com/LineageOS/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
